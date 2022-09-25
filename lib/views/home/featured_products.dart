576712a4b215c208995a8e2fc8d0d775bc04bb65
Sarthak <sarthak123parajuli@gmail.com>
2022-09-25 21:43:11 +0545

    featured product page completed

--- lib/views/home/featured_products.dart
+++ lib/views/home/featured_products.dart
@@ -1,16 +1,41 @@
 import 'package:aceshop/constraints/catrgories_temp.dart';
 import 'package:aceshop/constraints/constraints.dart';
 import 'package:aceshop/constraints/product_details_temp.dart';
+import 'package:aceshop/models/product_model/product_model.dart';
 import 'package:aceshop/views/category/category_page.dart';
 import 'package:aceshop/views/widgets/product_listing.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
-class FeaturedProductView extends StatelessWidget {
+import '../../models/services/home/home_services.dart';
+
+class FeaturedProductView extends StatefulWidget {
   const FeaturedProductView({
     Key? key,
   }) : super(key: key);
+
+  @override
+  State<FeaturedProductView> createState() => _FeaturedProductViewState();
+}
+
+class _FeaturedProductViewState extends State<FeaturedProductView> {
+  List<Product>? prodList;
+  HomeServices homeServices = HomeServices();
+  @override
+  void initState() {
+    // TODO: implement initState
+    fetchCategoryProd();
+    super.initState();
+  }
+
+  fetchCategoryProd() async {
+    prodList = await homeServices.fetchCagegoryProds(
+      context: context,
+      category: 'Foods',
+    );
+    setState(() {});
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -45,21 +70,27 @@ class FeaturedProductView extends StatelessWidget {
           child: SizedBox(
             width: MediaQuery.of(context).size.width,
             height: MediaQuery.of(context).size.height * 0.35,
-            child: ListView.builder(
-                scrollDirection: Axis.horizontal,
-                // physics: NeverScrollableScrollPhysics(),
-                shrinkWrap: true,
-                itemCount: productDemo.length,
-                itemBuilder: ((context, index) {
-                  return ProductListing(
-                    prodName: productDemo.keys.elementAt(index),
-                    prodImage: productDemo.values.elementAt(index),
-                    prodPrice: priceRatingDemo.keys.elementAt(index),
-                    prodRating: priceRatingDemo.values.elementAt(index),
-                    randIndex: index,
-                    btnFunc: () {},
-                  );
-                })),
+            child: prodList?.length == 0
+                ? Center(
+                    child: Text('No Products found'),
+                  )
+                : ListView.builder(
+                    scrollDirection: Axis.horizontal,
+                    // physics: NeverScrollableScrollPhysics(),
+                    shrinkWrap: true,
+                    itemCount: prodList?.length,
+                    itemBuilder: ((context, index) {
+                      final prodData = prodList?[index];
+                      return ProductListing(
+                        prodName: prodData!.name,
+                        prodImage: prodData.images[0],
+                        prodPrice: prodData.price.toString(),
+                        prodRating: '3',
+                        randIndex: index,
+                        btnFunc: () {},
+                      );
+                    }),
+                  ),
           ),
         ),
       ],
